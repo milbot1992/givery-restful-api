1a6b5cc2532b9697bfddbe1e6c80f96b3efd89bc
Millie Ellis <millieellis@Millies-MacBook-Pro.local>
2024-06-19 15:14:25 +0100

    updated post to be accurate

--- test_recipes.py
+++ test_recipes.py
@@ -11,24 +11,24 @@ def test_post_recipe(client):
         "cost": 1000
     })
     assert response.status_code == 200
-    assert response.json["title"] == "Chicken Curry"
+    assert response.json["recipe"][0]["title"] == "Chicken Curry"
 
 def test_get_all_recipes(client, init_database):
     response = client.get("/recipes")
     assert response.status_code == 200
-    assert len(response.json) == 2
+    assert len(response.json["recipes"]) == 2
 
 def test_get_recipe(client, init_database):
     response = client.get("/recipes/1")
     assert response.status_code == 200
-    assert response.json["title"] == "Chicken Curry"
+    assert response.json["recipe"][0]["title"] == "Chicken Curry"
 
 def test_patch_recipe(client, init_database):
     response = client.patch("/recipes/1", json={
         "title": "Spicy Chicken Curry"
     })
     assert response.status_code == 200
-    assert response.json["title"] == "Spicy Chicken Curry"
+    assert response.json["recipe"][0]["title"] == "Spicy Chicken Curry"
 
 def test_delete_recipe(client, init_database):
     response = client.delete("/recipes/1")
